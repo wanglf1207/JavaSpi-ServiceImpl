7bdd2aee371572be438c544703c43d39c9288f04
wanglifeng <wanglf1207@163.com>
2018-11-25 11:14:35 +0800

    第一次提交

---
+++ src/main/java/com/demo/service/impl/ManSpeak.java
@@ -0,0 +1,9 @@
+package com.demo.service.impl;
+
+import com.demo.service.Speak;
+
+public class ManSpeak implements Speak {
+    public void speak() {
+        System.out.println("大声的说。。。");
+    }
+}
---
+++ src/main/java/com/demo/service/impl/WomanSpeak.java
@@ -0,0 +1,9 @@
+package com.demo.service.impl;
+
+import com.demo.service.Speak;
+
+public class WomanSpeak implements Speak {
+    public void speak() {
+        System.out.println("温柔的说。。。");
+    }
+}
